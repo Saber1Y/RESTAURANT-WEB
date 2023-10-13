30985cb770cf9259f909dd9521626c5219e91bff
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-13 23:39:02 +0100

    DELETE: STYLE FOLDERS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { AboutUs, Chef, Findus, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import {Navbar } from './components'
+import './App.scss';
 
 export default function App() {
   return (
