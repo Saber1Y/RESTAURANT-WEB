bcf46cc97b6af4816072c35707545e48158d1ac0
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-13 19:47:53 +0100

    CREATE: ASSESTS & CONSTANTS COMPONENT DIR

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React, { useState } from 'react'
+import AboutUs from './components/aboutUs/AboutUs'
+import Chef from './components/chef/Chef'
+import Findus from './components/findus/Findus'
+import Footer from './components/footer/Footer'
+import Gallery from './components/gallery/Gallery'
+import Header from './components/header/Header'
+import Intro from './components/intro/Intro'
+import Laurels from './components/laurels/Laurels'
+import Navbar from './components/navbar/Navbar'
+import SpecialMenu from './components/specialmenu/SpecialMenu'
 
 export default function App() {
   return (
+    <>
     <h1>hello</h1>
+    <AboutUs />
+    <Chef />
+    <Findus />
+    <Footer />
+    <Gallery />
+    <Header />
+    <Intro />
+    <Laurels />
+    <Navbar />
+    <SpecialMenu />
+</>
   )
 }
---
+++ src/components/findus/Findus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Findus = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Findus
