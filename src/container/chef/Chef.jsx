dfc4d8b5bd4711b582e1f109ba27462b21d2f81e
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-14 23:43:12 +0100

    CREATE:  COMPONENT CONTENT

--- src/container/chef/Chef.jsx
+++ src/container/chef/Chef.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
 
 const Chef = () => {
   return (
-    <div>
+    <div className='app__bg app_wrapper section__padding'>
+      <div className='app__wrapper_img app__wrapper-reverse'>
+       <img src={images.chef} alt="chef" />
+      </div>
       
     </div>
   )
