c9cd7154d5d23e32a66c7811d0f680745b96999c
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-14 17:03:45 +0100

    CREATE & UPDATE: FIXED STYLING FOR COMPONENTS

--- src/container/aboutUs/AboutUs.jsx
+++ src/container/aboutUs/AboutUs.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { images } from '../../constants'
+import './about.scss'
 
 const AboutUs = () => {
   return (
-    <div>
-      
+    <div className='app__aboutuas app__bg flex__center section__padding' id='about'>
+      <div className="app_aboutus-overlay flex__center">
+        <img src={images.G} alt="G" />
+      </div>
+
     </div>
   )
 }
