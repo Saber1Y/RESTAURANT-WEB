8fdf04c0ac6d26d1526df4b2246fc5f51d3018ce
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-14 16:21:23 +0100

    UPDATE STYLING FOR HEADER

--- src/components/SubHeading/Subheading.jsx
+++ src/components/SubHeading/Subheading.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { images } from '../../constants'
 
-const Subheading = () => {
+
+const Subheading = ({ title }) => {
   return (
     <div>
-      
+      <p className="p_cormorant">{title}</p>
+      <img src={images.spoon} alt="spoon" className='spoon__img' />
     </div>
   )
 }
--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { images } from '../../constants'
 import './header.scss';
+import Subheading from '../../components/SubHeading/Subheading';
 
 const Header = () => {
   return (
-    <div>
-      im an header
+    <div className='app__header app__wrapper section__padding' id='home'>
+      <div className="app__wrapper_info">
+        <Subheading title="Chase the New Flavour" />
+        <h1 className="app__header-h1">The Key to Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type="button" className="custom__button">Explore Menu</button>
+      </div>
+      <div className="app__wrapper_img">
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
