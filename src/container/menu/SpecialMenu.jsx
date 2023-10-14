4e78c91d8eb17638e4c448c9747531281946edc2
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-14 18:17:16 +0100

    CREATE: Styling for menu

--- src/container/menu/SpecialMenu.jsx
+++ src/container/menu/SpecialMenu.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import { images } from '../../constants'
+import { SubHeading, MenuItem } from '../../components'
+import { images, data } from '../../constants'
 import './specialmenu.scss'
 
 const SpecialMenu = () => {
   return (
-    <div>
-      
+    <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your palatte" />
+        <h1 className='headtext__cormorant'>Today's Special</h1>
+      </div>
+      <div className="app__specialMenu-menu">
+        <div className="app__specialMnu-menu_wineflex__center">
+          <p className='app__specialMenu-menu_heading'>Wine & Beer </p>
+          <div className="app__specialmenu-menu_items">
+               
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
