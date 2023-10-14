848446089ecf528431f4da1c4bc673c9e16056d5
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-14 12:45:41 +0100

    UPDATE: NavBar style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-// import { MdOutlineResturantMenu } from 'react-icons/md'
+// import { MdOutlineRestaurantMenu } from 'react-icons/md'
 
 import images from '../../constants/images'
 const Navbar = () => {
