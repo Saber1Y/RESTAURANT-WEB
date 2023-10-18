6b6bfdf55c72b13eb9ef642041afa17bad77eaa1
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-18 06:25:30 +0100

    SCSS

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React,{ useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { MdOutlineRestaurantMenu } from 'react-icons/md'
 import images from '../../constants/images'
-import './navbar.css';
+import './navbar.scss';
 
 
 const Navbar = () => {
